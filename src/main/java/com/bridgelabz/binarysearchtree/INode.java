6758296d20fc3447c5607eb557dee1aeb36af6c4
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-02-02 14:00:42 +0530

    Added : Create a Binary Search Tree

---
+++ src/main/java/com/bridgelabz/binarysearchtree/INode.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.binarysearchtree;
+
+public class INode<T extends Comparable<T>> {
+	
+	T key;
+	INode<T> left, right;
+	
+	public INode(T item) {
+		
+		key = item;
+		left= right = null;
+	}
+}
